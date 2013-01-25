016f7b74744211e4fc72d30d37c8f8977d6c730c
George Reese <george.reese@imaginary.com>
2013-01-25 08:39:27 -0600

    Added tagging methods

--- src/main/java/org/dasein/cloud/mock/compute/image/MockImageSupport.java
+++ src/main/java/org/dasein/cloud/mock/compute/image/MockImageSupport.java
@@ -656,7 +656,22 @@ public class MockImageSupport implements MachineImageSupport {
     }
 
     @Override
-    public void updateTags(@Nonnull String imageId, @Nonnull Tag... tags) throws CloudException, InternalException {
+    public void updateTags(@Nonnull String vmId, @Nonnull Tag... tags) throws CloudException, InternalException {
+        // NO-OP
+    }
+
+    @Override
+    public void updateTags(@Nonnull String[] vmIds, @Nonnull Tag... tags) throws CloudException, InternalException {
+        // NO-OP
+    }
+
+    @Override
+    public void removeTags(@Nonnull String vmId, @Nonnull Tag... tags) throws CloudException, InternalException {
+        // NO-OP
+    }
+
+    @Override
+    public void removeTags(@Nonnull String[] vmIds, @Nonnull Tag... tags) throws CloudException, InternalException {
         // NO-OP
     }
 
